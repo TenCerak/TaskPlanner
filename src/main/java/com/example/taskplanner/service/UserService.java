1d0e61242687cfde85ce32837df0a4d8654a1f45
TenCerak <cerovsky2@gmail.com>
2025-01-02 13:08:51 +0100

    get current user

--- src/main/java/com/example/taskplanner/service/UserService.java
+++ src/main/java/com/example/taskplanner/service/UserService.java
@@ -10,4 +10,6 @@ public interface UserService extends UserDetailsService {
     User findByUsername(String username);
 
     void save(User user);
+
+    User getCurrentUser();
 }
--- src/main/java/com/example/taskplanner/service/UserServiceImpl.java
+++ src/main/java/com/example/taskplanner/service/UserServiceImpl.java
@@ -4,6 +4,8 @@ import com.example.taskplanner.model.User;
 import com.example.taskplanner.repository.UserRepository;
 import com.example.taskplanner.security.PlannerUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -35,5 +37,14 @@ public class UserServiceImpl implements UserService {
     @Override
     public void save(User user) {
         userRepository.save(user);
+    }
+
+    @Override
+    public User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails userDetails) {
+            return findByUsername(userDetails.getUsername());
+        }
+        return null;
     }
 }
