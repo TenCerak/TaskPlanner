74e489d890dde9b26d1cefeaa92de8b42e8a535c
TenCerak <cerovsky2@gmail.com>
2025-01-02 13:08:18 +0100

    security

--- src/main/java/com/example/taskplanner/security/SecurityConfig.java
+++ src/main/java/com/example/taskplanner/security/SecurityConfig.java
@@ -32,8 +32,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/hello").permitAll()
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
 
                 )
                 .formLogin(Customizer.withDefaults())
