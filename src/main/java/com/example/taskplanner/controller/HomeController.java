ea81d7394fb2dfc7a9673281cd2088a2437b9855
TenCerak <cerovsky2@gmail.com>
2025-01-04 23:05:27 +0100

    změna home controlleru

--- src/main/java/com/example/taskplanner/controller/HomeController.java
+++ src/main/java/com/example/taskplanner/controller/HomeController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+
     @GetMapping("/")
-    public String homePage() {
-        return "home";
+    public String home() {
+        return "pages/home";
     }
 }
