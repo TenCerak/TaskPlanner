97f1b616851f0c3be23cbc256cbf685eea6cb95e
TenCerak <cerovsky2@gmail.com>
2025-01-08 10:37:21 +0100

    lepší testovací data

--- src/main/java/com/example/taskplanner/TaskPlannerApplication.java
+++ src/main/java/com/example/taskplanner/TaskPlannerApplication.java
@@ -1,7 +1,11 @@
 package com.example.taskplanner;
 
+import com.example.taskplanner.model.Category;
+import com.example.taskplanner.model.Tag;
 import com.example.taskplanner.model.Task;
 import com.example.taskplanner.model.User;
+import com.example.taskplanner.service.CategoryService;
+import com.example.taskplanner.service.TagService;
 import com.example.taskplanner.service.TaskService;
 import com.example.taskplanner.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,18 +15,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class TaskPlannerApplication {
 
-    private TaskService taskService;
-    private UserService userService;
-    private PasswordEncoder passwordEncoder;
+    private final TaskService taskService;
+    private final UserService userService;
+    private final TagService tagService;
+    private final CategoryService categoryService;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public TaskPlannerApplication(TaskService taskService, UserService userService, PasswordEncoder passwordEncoder) {
+    public TaskPlannerApplication(TaskService taskService, UserService userService, TagService tagService, CategoryService categoryService, PasswordEncoder passwordEncoder) {
         this.taskService = taskService;
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
+        this.tagService = tagService;
+        this.categoryService = categoryService;
     }
 
     @Bean
@@ -32,15 +43,32 @@ public class TaskPlannerApplication {
             var admin = addUser("admin", "admin", "ADMIN");
             addUser("user", "heslo", "USER");
 
-            var t1 = addTask("Task 1", "Description 1", admin, null);
-            addTask("Task 2", "Description 2", admin, null);
-            addTask("Task 3", "Description 3", admin, null);
+            var category = new Category();
+            category.setName("Category 1");
+            categoryService.save(category);
 
-            var t11 = addTask("Task 1.1", "Description 1.1", admin, t1);
-            addTask("Task 1.2", "Description 1.2", admin, t1);
-            addTask("Task 1.3", "Description 1.3", admin, t1);
+            var tag = new Tag();
+            tag.setName("Tag 1");
+            tagService.save(tag);
 
-            var t111 = addTask("Task 1.1.1", "Description 1.1.1", admin, t11);
+            var tag2 = new Tag();
+            tag2.setName("Tag 2");
+            tagService.save(tag2);
+
+            var t1 = addTask("Task 1", "Description 1", admin, null,category,null);
+            t1.getTags().add(tag);
+            t1.getTags().add(tag2);
+            taskService.save(t1);
+            addTask("Task 2", "Description 2", admin, null,null,null);
+            addTask("Task 3", "Description 3", admin, null,category,null);
+
+            var t11 = addTask("Task 1.1", "Description 1.1", admin, t1,null,null);
+            t11.getTags().add(tag);
+            taskService.save(t11);
+            addTask("Task 1.2", "Description 1.2", admin, t1,null,null);
+            addTask("Task 1.3", "Description 1.3", admin, t1,category,null);
+
+            var t111 = addTask("Task 1.1.1", "Description 1.1.1", admin, t11,null,null);
         };
     }
 
@@ -58,7 +86,7 @@ public class TaskPlannerApplication {
 
     }
 
-    private Task addTask(String title, String description, User user, Task parentTask) {
+    private Task addTask(String title, String description, User user, Task parentTask, Category category, List<Tag> tags) {
 
         Task task = new Task();
         task.setTitle(title);
@@ -66,6 +94,12 @@ public class TaskPlannerApplication {
         task.setDescription(description);
         if (parentTask != null) {
             task.setParentTask(parentTask);
+        }
+        if (category != null) {
+            task.setCategory(category);
+        }
+        if (tags != null) {
+            task.setTags(tags);
         }
         taskService.save(task);
 
